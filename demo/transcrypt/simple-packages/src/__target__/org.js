94f816f3af467b5be96476568f570c8122da801e
Eric Snow <ericsnowcurrently@gmail.com>
2019-01-15 11:46:20 -0700

    Drop build artifacts.

--- demo/transcrypt/simple-packages/src/__target__/org.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-// Transcrypt'ed from Python, 2019-01-15 11:01:47
-var org_transcrypt___runtime___js_1 = require("./org.transcrypt.__runtime__.js");
-var __name__ = 'org';
-org_transcrypt___runtime___js_1.print('org');
-//# sourceMappingURL=org.map
